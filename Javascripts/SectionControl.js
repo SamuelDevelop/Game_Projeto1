2d15c22b3e2a81e62be15d9acd357eda9c7fd487
SamuelDevelop <ssoaresdossantos088@gmail.com>
2025-08-19 15:25:29 -0300

    Programação da Estrutura básico  de algo que virá ser Inventário

--- Javascripts/SectionControl.js
+++ Javascripts/SectionControl.js
@@ -6,32 +6,22 @@ console.log(SECOES);
 /* Setup de Funções*/
 
 function mostrarSecao(i, vetor){
-    if(i < vetor.length){
+    
+    let dadosJogador = carregarDados() || {};
+    preencherInventario(dadosJogador, i);
 
+    if(i < vetor.length){
         vetor.forEach(secao => {
             if(secao.style.display != "none"){
-                setTimeout(()=>{
-                    secao.style.opacity = 0;
-                }, 200);
-                setTimeout(()=>{
                     secao.style.display = "none";
-                }, 500);
             }
         });
-        setTimeout(()=>{
-            vetor[i].style.display = "flex";
-        }, 900);
-        setTimeout(()=>{
-            vetor[i].style.opacity = 1;
-        }, 1200);
-        
+        vetor[i].style.display = "flex";
     }
+
     else{
         let ultimo = vetor.length - 1;
         vetor[ultimo].style.display = "flex";
     }
 }
 
-
-
-mostrarSecao(0, SECOES)
